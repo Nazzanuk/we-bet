a79948fbd89ee49d328b4ce2599474fbe087266d
Nathan N <nazzanuk@gmail.com>
2015-10-04 18:20:44 +0100

    use heroku env port

--- server/server.js
+++ server/server.js
@@ -5,9 +5,8 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
     var host = server.address().address;
-    var port = server.address().port;
-
     console.log('Example app listening at http://%s:%s', host, port);
 });
